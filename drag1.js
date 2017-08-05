de79f6657c389b898682089d5f4d22297d9adc33
tuhoude <984367391@qq.com>
2017-08-05 15:02:41 +0800

    change drag1.js

--- drag1.js
+++ drag1.js
@@ -4,6 +4,8 @@ function drag(id){
 	var obj=document.getElementById(id);
 	var disX=0;
 	var disY=0;
-	obj.onmousedown=function(){
+	obj.onmousedown=function(ev){
+		disX=ev.pangeX-obj.offsetLeft;
+		disY=ev.pangeY-obj.offsetLeft;
 	}
 }
