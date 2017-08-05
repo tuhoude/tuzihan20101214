b6aaea7bd9c36230ad459cf3f3aa38f50d9fc64d
tuhoude <984367391@qq.com>
2017-08-05 15:57:52 +0800

    change drag1.js from new1

--- drag1.js
+++ drag1.js
@@ -7,5 +7,9 @@ function drag(id){
 	obj.onmousedown=function(ev){
 		disX=ev.pangeX-obj.offsetLeft;
 		disY=ev.pangeY-obj.offsetLeft;
+		document.onmousemove=function(){
+		  obj.style.left=ev.clientX-disX+'px';
+		  obj.style.top=ev.clientY-disY+'px';
+		}
 	}
 }
